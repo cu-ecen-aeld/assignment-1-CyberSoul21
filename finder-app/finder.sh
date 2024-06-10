45e1dd7e5e04f2964b0a4eb400ed955b20eba891
wjalmarior <wilson.jar21@gmail.com>
2024-06-09 22:55:08 -0500

    Shell script finder.sh created

---
+++ finder-app/finder.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#Author: Wilson Javier Almario R.
+#Date: 09/06/2024
+#Description: Receive a path and one string the program
+#returns how many file and subdirectories there are and how many matches
+
+filesdir=$1
+searchstr=$2
+if [ -z $filesdir ] || [ -z $searchstr ]
+then
+    exit 1
+fi
+
+if [ -d $filesdir ]
+then
+    numberFiles=$( ls $filesdir | wc -l )
+    NumberMatchs=$(grep -r $searchstr $filesdir | wc -l)
+    echo "The number of files are ${numberFiles} and the number of matching lines are ${NumberMatchs}"
+else
+    echo $filesdir "Does not exist"
+    exit 1
+fi
